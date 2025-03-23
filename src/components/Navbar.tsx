6c8697ff26e40b24fc24e41b5c48d7bd875fadcb
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-03-23 15:43:49 +0530

    Added Auth things :)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,16 @@
-
-export default function Navbar() {
+import {SignInButton, SignUpButton, SignedIn, SignedOut, UserButton} from "@clerk/nextjs"
+const Navbar = () => {
   return (
-    <div className="flex justify-center items-center">
-      <p>Navbar</p>
+    <div className="flex items-center justify-end gap-2 py-1 px-7">
+      <SignedOut>
+        <SignInButton/>
+        <SignUpButton/>
+      </SignedOut>
+      <SignedIn>
+        <UserButton/>
+      </SignedIn>
     </div>
   )
 }
+
+export default Navbar;
