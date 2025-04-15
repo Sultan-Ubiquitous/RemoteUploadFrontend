6e85b376bb4605921c2b88e081c58c090a78993f
Sultan-Ubiquitous <azimakhorajiya@gmail.com>
2025-04-15 20:26:52 +0530

    Added things, will set up the background till I just need to put in the engine and get the car running

--- src/app/onboarding/_actions.ts
+++ src/app/onboarding/_actions.ts
@@ -1,27 +1,41 @@
 'use server';
 
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import axios from "axios";
 
 export const completeOnboarding =  async (formData: FormData) => {
   const {userId} = await auth();
+  const {getToken} = await auth();
+  const token = await getToken();
+  
   if(!userId){
     return {message: 'No Logged In User'}
   }
 
   const client = await clerkClient();
+  const onlyId = userId.slice(5);
   const user = await client.users.getUser(userId);
   const userType = formData.get('userType') as 'owner' | 'member' | null;
   const orgName = formData.get('orgName') as string | null;
   const orgSlug = formData.get('orgSlug') as string | null;
-  const userEmail = user.emailAddresses;
-  const name = user.fullName;
-
+  
+  //create first user
   try {
-    //Create a user first with email and userId
+    const response = await axios.post('http://localhost:8060/onboard/create_user', {
+      email: user.emailAddresses[0].emailAddress,
+      userId: userId
+    }, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+    console.log('User created: ', response.data);
+    return response.data;
 
   } catch (error) {
-    
+    console.error("Failed to make an user: ", error);
   }
+
   
 
   if(!userType){
@@ -30,9 +44,9 @@ export const completeOnboarding =  async (formData: FormData) => {
   
   
   try {
-    const metadata: { onboardingComplete: boolean;}  = {
-      onboardingComplete: true,
-    }  
+    // const metadata: { onboardingComplete: boolean;}  = {
+    //   onboardingComplete: true,
+    // }  
 
     if(userType === 'owner') {
       if(!orgName || !orgSlug) {
@@ -40,17 +54,22 @@ export const completeOnboarding =  async (formData: FormData) => {
       }
       
       //axios.post userId and orgName and orgSlug  to /create_organization because only owner will be making this request
+      console.log('Owner created hihi');
+      
+      return;
     }
 
     if(userType === 'member'){
       //axios.post 
+      console.log('Member created hihi');
+      return;
     }
 
-    const updateUser = await client.users.updateUser(userId, {
-      publicMetadata: metadata
-    });
+    // const updateUser = await client.users.updateUser(userId, {
+    //   publicMetadata: metadata
+    // });
 
-    return{message: updateUser.publicMetadata}
+    return{message: "Did some shit"}
 
   } catch (error) {
     return { error: 'There was an error updating the user metadata.' }
